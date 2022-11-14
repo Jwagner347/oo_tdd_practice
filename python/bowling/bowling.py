23cb4a39258a7692e2870ae623773bcd88a71ac4
Jeff Wagner <jeff.wagner@swipesense.com>
2022-11-14 07:53:45 -0600

    creates frames for a game

--- python/bowling/bowling.py
+++ python/bowling/bowling.py
@@ -1,7 +1,36 @@
+class Game:
+    def __init__(self, frame_line) -> None:
+        self._frames = self.create_frames(frame_line)
+        
+    def create_frames(self, frame_line):
+        frames_dict = {}
+        frames = frame_line.split("|")
+        for i, val in enumerate(frames):
+            if val != "":
+                frame = Frame(frames[i])
+                frames_dict[i+1] = frame
+        # {key: value for (key, value) in frames}
+        return frames_dict
+    
+    def get_frame(self, frame_num):
+        return self._frames[frame_num]
+
 class Frame:
     def __init__(self, pins_down) -> None:
-        self._pins_down = pins_down
+        # refactor this to be a method
+        first_pin_string = list(pins_down)[0]
+        second_pin_string = list(pins_down)[1]
         
-    def calculate_frame_score(self):
-        first_pin, second_pin = list(self._pins_down)
-        return int(first_pin) + int(second_pin)
+        if first_pin_string == '-':
+            first_pin_string = 0
+        if second_pin_string == '-':
+            second_pin_string = 0
+
+        self._first_pin = int(first_pin_string)
+        self._second_pin = int(second_pin_string)
+        
+    # def calculate_pins_only_score(self):
+    #     first_pin, second_pin = list(self._pins_down)
+    #     if first_pin == '/' or second_pin == '/':
+    #         return 10
+    #     return int(first_pin) + int(second_pin)
--- python/bowling/bowling_test.py
+++ python/bowling/bowling_test.py
@@ -1,8 +1,18 @@
 import pytest
 
-from bowling import Frame
+from bowling import Game, Frame
 
-def test_can_calculate_total_pins():
-    ten_pin_frame = Frame("45")
-    assert ten_pin_frame.calculate_frame_score() == 9
+def test_can_parse_both_pins():
+    frame = Frame("45")
+    assert frame._first_pin == 4
+    assert frame._second_pin == 5
+    
+def test_creates_correct_number_of_frames():
+    frame_line = '9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||'
+    game = Game(frame_line)
+    
+    assert game.get_frame(1)._first_pin == 9
+    assert game.get_frame(1)._second_pin == 0
 
+def test_can_calculate_frame_score_ints_only():
+    pass
